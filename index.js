3e4a3165ef8950b15c7f1a27bfa80f4c71af0a9b
AsociTon <asociton@outlook.com>
2019-10-19 22:53:43 +0530

    Add basic socket.io boiler plate.
    Creating a basic socket.io chat server for sending realtime data
    between server and clients.

---
+++ index.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+        io.emit('chat message', 'Oops, we lost one user');
+    });
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(7000, () => {
+    console.log('listening on *:7000');
+});
